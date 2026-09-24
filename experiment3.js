63d4f9388d010e441e8ccb03975d874dfb3c3993
pranjal200420 <pranjal102314@gmail.com>
2026-09-24 11:45:25 +0530

    experiment3 commited

---
+++ experiment3.js
@@ -0,0 +1,10 @@
+import http from 'http';
+const server = http.createServer((req, res) => {
+    res.statusCode = 200; res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+
+server.listen(3000, () => {
+    console.log('Server running at h p://localhost:3000');
+}); 
