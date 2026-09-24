f43b5d43fa7e496e6acfd533434d8fcff8e3a182
pranjal200420 <pranjal102314@gmail.com>
2026-09-24 11:28:26 +0530

    experiment1-3 commited

---
+++ experiment1-3.js
@@ -0,0 +1,5 @@
+console.log("Start");
+setTimeout(() => { console.log("setTimeout"); }, 0);
+setImmediate(() => { console.log("setImmediate"); });
+process.nextTick(() => { console.log("process.nextTick"); });
+console.log("End"); 
