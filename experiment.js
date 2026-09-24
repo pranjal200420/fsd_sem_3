cd12fe12bbce943d7aca64627637394d7c43feb6
pranjal200420 <pranjal102314@gmail.com>
2026-09-24 11:07:33 +0530

    first experiment

---
+++ experiment.js
@@ -0,0 +1,8 @@
+const EventEmitter = require('events');
+const myEmitter = new EventEmitter();
+myEmitter.on('greet', (name) => {
+    console.log('Hello, ' + name + '! Welcome to Node.js.');
+});
+myEmitter.on('exit', () => { console.log('Application Closed.'); });
+myEmitter.emit('greet', 'Ayush');
+myEmitter.emit('exit'); 
